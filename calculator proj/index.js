621a45c84cbc2b335315ed89b72d83f2d8ee2c08
Mark Mendoza <mark0andrei2mendoza@gmail.com>
2023-08-22 20:00:10 +0800

    Split JS from HTML for num

---
+++ calculator proj/index.js
@@ -0,0 +1,33 @@
+function one(){
+    document.getElementById("display").value += "1"
+}
+function two(){
+    document.getElementById("display").value += "2"
+}
+function three(){
+    document.getElementById("display").value += "3"
+}
+function four(){
+    document.getElementById("display").value += "4"
+}
+function five(){
+    document.getElementById("display").value += "5"
+}
+function six(){
+    document.getElementById("display").value += "6"
+}
+function seven(){
+    document.getElementById("display").value += "7"
+}
+function eight(){
+    document.getElementById("display").value += "8"
+}
+function nine(){
+    document.getElementById("display").value += "9"
+}
+function zero(){
+    document.getElementById("display").value += "0"
+}
+function dzero(){
+    document.getElementById("display").value += "00"
+}
