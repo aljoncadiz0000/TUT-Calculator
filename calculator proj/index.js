8cf79dd094345ca91350afc4e1ced04373b37623
Mark Mendoza <mark0andrei2mendoza@gmail.com>
2023-08-28 21:04:19 +0800

    bug fixes (decimal point) & continous operations

--- calculator proj/index.js
+++ calculator proj/index.js
@@ -1,14 +1,13 @@
 let display = document.getElementById("display");
 
 function dsplyNum(val){
+
+    const zroOprtns = ['+0','-0','*0','/0'];
+    const last2Char = display.value.slice(-2);
+
     if (display.value=="0"){
         display.value = val
-    } else if (
-        (display.value.slice(-2) =="+0")||
-        (display.value.slice(-2) =="-0")||
-        (display.value.slice(-2) =="*0")||
-        (display.value.slice(-2) =="/0")
-        ){
+    } else if (zroOprtns.includes(last2Char)){
         display.value = display.value.toString().slice(0,-1)
         display.value += val
     } else {
@@ -16,21 +15,17 @@ function dsplyNum(val){
     }
 }
 function dsplyZero(val){
+    
+    const oprtns = ['+','-','*','/'];
+    const lastChar = display.value.slice(-1);
+    const zroOprtns = ['+0','-0','*0','/0']
+    const last2Char = display.value.slice(-2);
+
     if (display.value=="0"){
         display.value = "0"
-    } else if (
-        (display.value.slice(-1) =="+")||
-        (display.value.slice(-1) =="-")||
-        (display.value.slice(-1) =="*")||
-        (display.value.slice(-1) =="/")
-        ){
+    } else if (oprtns.includes(lastChar)){
         display.value += "0"
-    } else if (
-        (display.value.slice(-2) =="+0")||
-        (display.value.slice(-2) =="-0")||
-        (display.value.slice(-2) =="*0")||
-        (display.value.slice(-2) =="/0")
-        ){
+    } else if (zroOprtns.includes(last2Char)){
         display.value += ""
     } else {
         display.value += val
@@ -47,13 +42,11 @@ function clr(){
     display.value = "0"
 }
 function dsplyOperation(val){
-    if (
-        (display.value.slice(-1) =="+")||
-        (display.value.slice(-1) =="-")||
-        (display.value.slice(-1) =="*")||
-        (display.value.slice(-1) =="/")||
-        (display.value.slice(-1) ==".")
-        ){
+
+    const oprtns = ['+','-','*','/'];
+    const lastChar = display.value.slice(-1);
+
+    if (oprtns.includes(lastChar)){
         display.value = display.value.toString().slice(0,-1)
         display.value += val
     } else if (
@@ -62,21 +55,38 @@ function dsplyOperation(val){
         (display.value.includes("*"))||
         (display.value.includes("/"))
         ){
-        display.value += ""
+        display.value = eval(display.value) + val
+    } else if (display.value.slice(-1) =="."){
+        display.value = display.value.toString().slice(0,-1)
+        display.value += val
     } else {
         display.value += val
     }
 }
 function dot(){
-    if (
-        (display.value.slice(-1) =="+")||
-        (display.value.slice(-1) =="-")||
-        (display.value.slice(-1) =="*")||
-        (display.value.slice(-1) =="/")||
-        (display.value=="")
-        ){
+    
+    const oprtns = ['+','-','*','/'];
+    const lastChar = display.value.slice(-1);
+    
+
+    if (oprtns.includes(lastChar)){
         display.value += "0."
     } else if (display.value.slice(-1) =="."){
+        display.value += ""
+    } else if (display.value.indexOf('.')>=
+              (display.value.indexOf('+')
+              +display.value.indexOf('-')
+              +display.value.indexOf('*')
+              +display.value.indexOf('/')+4) /*+4 is used to balance indexes*/
+              ){
+        display.value += ""
+    } else if ( (display.value.split('.').length==2)&&(display.value.includes('*'))||
+                (display.value.split('.').length==2)&&(display.value.includes('-'))||
+                (display.value.split('.').length==2)&&(display.value.includes('/'))||
+                (display.value.split('.').length==2)&&(display.value.includes('+'))
+                ){
+        display.value += "."
+    } else if (display.value.includes(".")){
         display.value += ""
     } else {
         display.value += "."
