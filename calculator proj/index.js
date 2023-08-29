be042a5cad283647f894144f9477d80833efc4d5
Mark Mendoza <mark0andrei2mendoza@gmail.com>
2023-08-29 21:23:14 +0800

    refactor code based on recommendations

--- calculator proj/index.js
+++ calculator proj/index.js
@@ -1,97 +1,84 @@
 let display = document.getElementById("display");
+const oprtns = ['+','-','*','/'];
+const zroOprtns = ['+0','-0','*0','/0'];
 
 function dsplyNum(val){
 
-    const zroOprtns = ['+0','-0','*0','/0'];
     const last2Char = display.value.slice(-2);
 
     if (display.value=="0"){
-        display.value = val
+        display.value = val;
     } else if (zroOprtns.includes(last2Char)){
-        display.value = display.value.toString().slice(0,-1)
-        display.value += val
+        display.value = display.value.toString().slice(0,-1);
+        display.value += val;
     } else {
-    display.value += val
+    display.value += val;
     }
 }
 function dsplyZero(val){
-    
-    const oprtns = ['+','-','*','/'];
+
     const lastChar = display.value.slice(-1);
-    const zroOprtns = ['+0','-0','*0','/0']
     const last2Char = display.value.slice(-2);
 
     if (display.value=="0"){
-        display.value = "0"
+        display.value = "0";
     } else if (oprtns.includes(lastChar)){
-        display.value += "0"
+        display.value += "0";
     } else if (zroOprtns.includes(last2Char)){
-        display.value += ""
+        return(display.value);
     } else {
-        display.value += val
+        display.value += val;
     }
 }
 function del(){
     if (display.value.length == 1){
-        display.value = "0"
+        display.value = "0";
     } else {
-    display.value = display.value.toString().slice(0,-1)
+        display.value = display.value.toString().slice(0,-1);
     }
 }
 function clr(){
-    display.value = "0"
+    display.value = "0";
 }
 function dsplyOperation(val){
 
-    const oprtns = ['+','-','*','/'];
     const lastChar = display.value.slice(-1);
 
     if (oprtns.includes(lastChar)){
-        display.value = display.value.toString().slice(0,-1)
-        display.value += val
+        display.value = display.value.toString().slice(0,-1);
+        display.value += val;
     } else if (
         (display.value.includes("+"))||
         (display.value.includes("-"))||
         (display.value.includes("*"))||
         (display.value.includes("/"))
         ){
-        display.value = eval(display.value) + val
+        equal + val;
     } else if (display.value.slice(-1) =="."){
-        display.value = display.value.toString().slice(0,-1)
-        display.value += val
+        display.value = display.value.toString().slice(0,-1);
+        display.value += val;
     } else {
-        display.value += val
+        display.value += val;
     }
 }
 function dot(){
     
-    const oprtns = ['+','-','*','/'];
     const lastChar = display.value.slice(-1);
     
-
     if (oprtns.includes(lastChar)){
         display.value += "0."
-    } else if (display.value.slice(-1) =="."){
-        display.value += ""
-    } else if (display.value.indexOf('.')>=
-              (display.value.indexOf('+')
-              +display.value.indexOf('-')
-              +display.value.indexOf('*')
-              +display.value.indexOf('/')+4) /*+4 is used to balance indexes*/
-              ){
-        display.value += ""
-    } else if ( (display.value.split('.').length==2)&&(display.value.includes('*'))||
-                (display.value.split('.').length==2)&&(display.value.includes('-'))||
-                (display.value.split('.').length==2)&&(display.value.includes('/'))||
-                (display.value.split('.').length==2)&&(display.value.includes('+'))
-                ){
-        display.value += "."
-    } else if (display.value.includes(".")){
-        display.value += ""
+    } else if (
+        display.value.indexOf('.')>=
+        (display.value.indexOf('+')
+        +display.value.indexOf('-')
+        +display.value.indexOf('*')
+        +display.value.indexOf('/')+4) /*+4 is used to balance indexes*/
+        ){
+        return(display.value) 
     } else {
         display.value += "."
     }
 }
 function equal(){
-    display.value = eval(display.value)
+    display.value = eval(display.value);
 }
